fd3dfe26bd4b1304293471b6d303b07e7a20a9ac
Fatih1966 <halitfatihkulac@gmail.com>
2021-04-13 09:17:38 +0300

    Add project files.

---
+++ Library/Class1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Library
+{
+
+    public enum Tip
+    {
+        MasaUstu,
+        Kablosuz,
+        Arac,
+        Cep,
+        Uydu
+    }
+
+    public enum Marka
+    {
+        Samsung,
+        IPhone,
+        Huawei,
+        Xiaomi,
+        General_Mobile,
+        LG,
+        Vestel,
+        Casper,
+    }
+
+    public enum IsletimSistemi
+    {
+        Android,
+        Symbian,
+        IOS,
+        BlackBerryOS,
+        Windows_Mobile,
+        Bada,
+        WebOS,
+        Landline,
+        Others
+    }
+
+    public enum ekran
+    {
+        Ekranyok,
+        Ucbucukinch,
+        Dortinch,
+        Dortnoktayediinch,
+        Besbucukinch,
+    }
+
+    public class Telefon
+    {
+
+        public Telefon()
+        {
+            ModelYili = DateTime.Now.Year;
+            Tip = Tip.Cep;
+            Marka = Marka.IPhone;
+            IsletimSistemi = IsletimSistemi.IOS;
+            Ekran = ekran.Dortinch;
+
+        }
+
+        public int ModelYili { get; set; }
+        public Tip Tip { get; set; }
+        public Marka Marka { get; set; }
+        public IsletimSistemi IsletimSistemi { get; set; }
+        public ekran Ekran { get; set; }
+
+        private int fiyat = 2000;
+        public int Fiyat
+        {
+            get
+            {
+                return fiyat;
+            }
+            set
+            {
+                if (value>1500)
+                    Fiyat = fiyat;
+                else
+                {
+                    Console.WriteLine("Yanlış deger girdiniz");
+                }
+            }
+        }
+    }
+
+    
+}
---
+++ TelefonHFK/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library;
+
+namespace TelefonHFK
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Telefon telefon = new Telefon();
+
+            //Iphone için
+            //constructor'daki default değerler
+            Console.WriteLine("Markası\t:\t" + telefon.Marka);
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Tipi\t:\t" + telefon.Tip);
+            Console.WriteLine("İsl.Sis\t:\t"+telefon.IsletimSistemi);
+            Console.WriteLine("Yılı\t:\t" + telefon.ModelYili);
+            //bu telefonun özel değerleri atanıyor
+            telefon.Ekran = ekran.Besbucukinch;
+            Console.WriteLine("Ekran\t:\t"+telefon.Ekran);
+            //fiyat değeri kontrol edildikten sonra yazdırılıyor.
+            Console.WriteLine("Fiyatı\t:\t"+telefon.Fiyat);
+
+        }
+    }
+}
